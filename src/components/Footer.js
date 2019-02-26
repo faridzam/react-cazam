70a0a99e2a9c418230a8fead02069d517760ba0e
faridzam <skinel123@gmail.com>
2019-02-26 09:59:06 +0700

    second commit

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+class Footer extends React.Component{
+    render(){
+        return(
+            <footer className="footer">
+                <div className="company-contact">
+                    - company location : xxxxxxxxxxxxxxxxxxxxxxxx<br/>
+                    - phone : +628xxxxxxxxxx<br/>
+                    - email : cazamproject@gmail.com<br/>
+                </div>
+                <div className="about-cazam">
+                    <h3>About Cazam</h3>
+                    <p>
+                        cazam adalah xxxxxxxxx<br/>
+                        xxxxxxxxxxxxxxxxxxxxxx<br/>
+                        xxxxxxxxxxxxxxxxxxxxxx<br/>
+                    </p>
+                </div>
+            </footer>
+        )
+    }
+}
+
+export default Footer
---
+++ src/components/OverlayMenu.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class OverlayMenu extends React.Component{
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default OverlayMenu
